7c7b0a342141f217b07af139024637cbdb5d999f
Arman <armankhan232019@gmail.com>
2025-07-22 18:22:43 +0530

    visa api first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+// visa-ServiceWorkerRegistration
+document.getElementById("countryDetails").addEventListener("click", function () {
+    window.location.href = "../t2-project/index.html";
+  });
+  
+  document.getElementById("visaCategories").addEventListener("click", function () {
+    window.location.href = "/visa.html";
+  });
+  
